91be68e354e5c68d96cd88f3494e13bb9f92ba2a
Jacques Verpoest <jacques.verpoest@gmail.com>
2025-04-06 16:22:15 +0200

    user edit mode

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,8 +5,12 @@ import Account from "../../components/Account/Account"
 import { fetchAccountsByUserId } from "../../api/accounts"
 import { useDispatch } from "react-redux"
 import { setUser } from "../../store/authSlice"
+import { useState } from "react"
 
 const Profile = () => {
+  const [editMode, setEditMode] = useState(true)
+  const [newUserFirstName, setNewUserFirstName] = useState("")
+  const [newUserLastName, setNewUserLastName] = useState("")
   const dispatch = useDispatch()
 
   const {
@@ -29,16 +33,64 @@ const Profile = () => {
 
   dispatch(setUser(user))
 
+  const handleCancelEdit = () => {
+    setNewUserFirstName("")
+    setNewUserLastName("")
+    setEditMode(false)
+  }
+
+  const handleSaveNewUser = () => {
+    const updatedUser = {
+      ...user,
+      firstName: newUserFirstName || user.firstName,
+      lastName: newUserLastName || user.lastName,
+    }
+    dispatch(setUser(updatedUser))
+    setEditMode(false)
+  }
+
   return (
     <main className="container__profile">
-      <div className="container__profile__header">
-        <h1>
-          Welcome back
-          <br />
-          {user.firstName} {user.lastName}
-        </h1>
-        <button className="edit-button">Edit Name</button>
-      </div>
+      <section className="container__profile__header">
+        <h1>Welcome back</h1>
+
+        {!editMode ? (
+          <>
+            <p className="user-name">
+              {user.firstName} {user.lastName}
+            </p>
+            <button className="edit-button" onClick={() => setEditMode(true)}>
+              Edit Name
+            </button>
+          </>
+        ) : (
+          <div className="edit-form">
+            <div className="edit-form__inputs">
+              <input
+                type="text"
+                value={newUserFirstName}
+                onChange={(e) => setNewUserFirstName(e.target.value)}
+                placeholder={user.firstName}
+              ></input>
+              <input
+                type="text"
+                value={newUserLastName}
+                onChange={(e) => setNewUserLastName(e.target.value)}
+                placeholder={user.lastName}
+              ></input>
+            </div>
+            <div className="edit-form__controls">
+              <div className="edit-form__controls__button" onClick={handleSaveNewUser}>Save</div>
+              <div
+                className="edit-form__controls__button"
+                onClick={handleCancelEdit}
+              >
+                Cancel
+              </div>
+            </div>
+          </div>
+        )}
+      </section>
       <section className="accounts">
         {accounts.map((account, index) => (
           <Account key={index} account={account} />
